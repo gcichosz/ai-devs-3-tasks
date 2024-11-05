73c1d90743e925ffcfba24b531391694e6b32c09
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-05 19:17:56 +0100

    feat: add send request skill

---
+++ src/skills/send-request/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+import { AppError } from '../../errors';
+
+export class SendRequestSkill {
+  async postRequest(url: string, data: Record<string, unknown>): Promise<Record<string, unknown>> {
+    try {
+      const response = await axios.post(url, JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw new AppError('Failed to send POST request', { error });
+    }
+  }
+}
