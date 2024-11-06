6687726d6fd441714ae744c25b0742a1eda4a2e4
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-06 18:28:45 +0100

    feat: add JSON parsing and processing functionality

--- src/json/index.ts
+++ src/json/index.ts
@@ -1,8 +1,36 @@
-// TODO: Load JSON from json.txt
-// TODO: Parse the JSON data
+import { promises as fs } from 'fs';
+
+type TestData = {
+  question: string;
+  answer: number;
+  test?: {
+    q: string;
+    a: string;
+  };
+};
+
+type CalibrationData = {
+  apikey: string;
+  description: string;
+  copyright: string;
+  'test-data': TestData[];
+};
+
+const loadJson = async (filePath: string): Promise<CalibrationData> => {
+  const fileContent = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(fileContent);
+};
+
 // TODO: Calculate number of tokens in test-data
 // TODO: Split test data in parts based on number of tokens
 // TODO: Send test data parts to LLM to fix it
 // TODO: Combine fixed test data parts
 // TODO: Replace API key placeholder with actual API key
 // TODO: Send fixed test data to API
+
+const main = async () => {
+  const jsonData = await loadJson('./src/json/json.txt');
+  console.log(jsonData['test-data'].length);
+};
+
+main();
