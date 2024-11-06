1170c0e2303076250da7723d85087427d6283374
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-06 18:14:18 +0100

    chore: add JSON task solution plan

---
+++ src/json/index.ts
@@ -0,0 +1,8 @@
+// TODO: Load JSON from json.txt
+// TODO: Parse the JSON data
+// TODO: Calculate number of tokens in test-data
+// TODO: Split test data in parts based on number of tokens
+// TODO: Send test data parts to LLM to fix it
+// TODO: Combine fixed test data parts
+// TODO: Replace API key placeholder with actual API key
+// TODO: Send fixed test data to API
