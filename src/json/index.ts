18cf814182cb32ba4404db44330302951c9ca067
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-06 18:45:28 +0100

    feat: calculate number of tokens in test data

--- src/json/index.ts
+++ src/json/index.ts
@@ -1,5 +1,7 @@
 import { promises as fs } from 'fs';
 
+import { TokenizerSkill } from '../skills/tokenizer';
+
 type TestData = {
   question: string;
   answer: number;
@@ -21,16 +23,22 @@ const loadJson = async (filePath: string): Promise<CalibrationData> => {
   return JSON.parse(fileContent);
 };
 
-// TODO: Calculate number of tokens in test-data
+const calculateTokens = async (data: TestData[]): Promise<number> => {
+  const tokenizer = new TokenizerSkill();
+  const jsonString = JSON.stringify(data);
+  return await tokenizer.countTokens(jsonString, 'gpt-4o');
+};
+
 // TODO: Split test data in parts based on number of tokens
 // TODO: Send test data parts to LLM to fix it
 // TODO: Combine fixed test data parts
 // TODO: Replace API key placeholder with actual API key
 // TODO: Send fixed test data to API
-
 const main = async () => {
   const jsonData = await loadJson('./src/json/json.txt');
   console.log(jsonData['test-data'].length);
+  const tokenCount = await calculateTokens(jsonData['test-data']);
+  console.log(`Number of tokens in test-data: ${tokenCount}`);
 };
 
 main();
