a8e43da0b40a537485f4c1b4ea1dd157ef832581
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-09 18:35:53 +0100

    refactor: type process.env

---
+++ src/environment.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    OPENAI_API_KEY: string;
+    ANTHROPIC_API_KEY: string;
+    FIRECRAWL_API_KEY: string;
+    AI_DEVS_API_KEY: string;
+    OPENAI_PROJECT_ID: string;
+  }
+}
