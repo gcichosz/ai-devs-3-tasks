ffd6b7a3cf39457b451c082df26daf678e0f6609
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-09 18:40:03 +0100

    feat: get sensitive data

--- src/censorship/index.ts
+++ src/censorship/index.ts
@@ -1,4 +1,5 @@
-// TODO: Fetch data from centrala.ag3nts.org/data/KLUCZ/cenzura.txt
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
 // TODO: Add HTTP headers handling for additional information
 // TODO: Add Llama skill
 // TODO: Send censorship request to Llama skill (replace sensitive data with "CENZURA"):
@@ -9,6 +10,12 @@
 // TODO: Ensure preservation of all punctuation and spacing
 // TODO: Implement sending censored data to centrala.ag3nts.org/report
 
-const censor = async () => {};
+const censor = async () => {
+  const sendRequestSkill = new SendRequestSkill();
+  const sensitiveData = await sendRequestSkill.getRequest(
+    `https://centrala.ag3nts.org/data/${process.env.AI_DEVS_API_KEY}/cenzura.txt`,
+  );
+  console.log(sensitiveData);
+};
 
 censor();
