7521bb20186e3eb4a97a84a19fcbe17c6304b5b6
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-09 18:35:31 +0100

    chore: add censorship task solution plan

---
+++ src/censorship/index.ts
@@ -0,0 +1,14 @@
+// TODO: Fetch data from centrala.ag3nts.org/data/KLUCZ/cenzura.txt
+// TODO: Add HTTP headers handling for additional information
+// TODO: Add Llama skill
+// TODO: Send censorship request to Llama skill (replace sensitive data with "CENZURA"):
+//       - first name and last name
+//       - age
+//       - city
+//       - street name and number
+// TODO: Ensure preservation of all punctuation and spacing
+// TODO: Implement sending censored data to centrala.ag3nts.org/report
+
+const censor = async () => {};
+
+censor();
