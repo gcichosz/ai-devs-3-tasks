f4469b6fd75735ac69e08b14019fee6e6267a92c
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-11 12:09:27 +0100

    feat: add speech to text skill

---
+++ src/skills/speech-to-text/SpeechToTextSkill.ts
@@ -0,0 +1,20 @@
+import Groq from 'groq-sdk';
+import { toFile } from 'openai';
+
+export class SpeechToTextSkill {
+  private readonly groq: Groq;
+
+  constructor(private readonly groqApiKey: string) {
+    this.groq = new Groq({ apiKey: groqApiKey });
+  }
+
+  async transcribe(speech: Buffer): Promise<string> {
+    const transcription = await this.groq.audio.transcriptions.create({
+      file: await toFile(speech, 'speech.mp3'),
+      model: 'whisper-large-v3',
+      language: 'pl',
+    });
+
+    return transcription.text;
+  }
+}
