96318285cfb356bca0578450f51160fb1016a260
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-11 15:57:19 +0100

    feat: use all speech files to generate context

--- src/interrogation/index.ts
+++ src/interrogation/index.ts
@@ -4,7 +4,6 @@ import { OpenAISkill } from '../skills/open-ai/open-ai-skill';
 import { SpeechToTextSkill } from '../skills/speech-to-text/SpeechToTextSkill';
 import { answerQuestionPrompt, irrelevantInformationFilterPrompt } from './prompts';
 
-// TODO: Create a function that combines all transcriptions into a shared context
 // TODO: Add logic to check testimony consistency, with special attention to Rafal's testimony
 // TODO: Implement chain-of-thought prompting for better model reasoning
 // TODO: Implement sending response to headquarters (endpoint /report) with task name 'mp3'
@@ -13,31 +12,54 @@ const main = async () => {
   const speechToTextSkill = new SpeechToTextSkill(process.env.GROQ_API_KEY);
   const openAiSkill = new OpenAISkill(process.env.OPENAI_API_KEY);
 
-  // TODO: Use all the files
   const cacheFile = await fs.readFile('./src/interrogation/cache.json', 'utf-8');
   const cache = JSON.parse(cacheFile);
 
-  let transcription = '';
-  if (!cache.adam) {
-    const speech = await fs.readFile('./src/interrogation/interrogation-files/adam.m4a');
-    transcription = await speechToTextSkill.transcribe(speech);
-    cache.adam = transcription;
-    await fs.writeFile('./src/interrogation/cache.json', JSON.stringify(cache, null, 2));
-  } else {
-    transcription = cache.adam;
+  const files = await fs.readdir('./src/interrogation/interrogation-files');
+  console.log(files);
+
+  const transcriptions = [];
+  for (const fileName of files) {
+    const personName = fileName.split('.')[0];
+    if (!cache[personName]) {
+      const speech = await fs.readFile(`./src/interrogation/interrogation-files/${fileName}`);
+      const transcription = await speechToTextSkill.transcribe(speech);
+      console.log(transcription);
+      cache[personName] = transcription;
+      transcriptions.push(transcription);
+      await fs.writeFile('./src/interrogation/cache.json', JSON.stringify(cache, null, 2));
+    } else {
+      transcriptions.push(cache[personName]);
+    }
   }
-  console.log(transcription);
+  console.log(transcriptions);
 
-  const irrelevantInformationResponse = await openAiSkill.completionFull([
-    { role: 'system', content: irrelevantInformationFilterPrompt },
-    { role: 'user', content: transcription },
-  ]);
-  console.log(irrelevantInformationResponse.choices[0].message.content);
+  const isRelevantDecisions = await Promise.all(
+    transcriptions.map(async (transcription) => {
+      const irrelevantInformationResponse = await openAiSkill.completionFull([
+        { role: 'system', content: irrelevantInformationFilterPrompt },
+        { role: 'user', content: transcription },
+      ]);
+      console.log(irrelevantInformationResponse.choices[0].message.content);
+      return irrelevantInformationResponse.choices[0].message.content?.includes('relevant: 1') || false;
+    }),
+  );
+  console.log(isRelevantDecisions);
 
-  const answerQuestionResponse = await openAiSkill.completionFull([
-    { role: 'system', content: answerQuestionPrompt(`- ${transcription}`) },
-    { role: 'user', content: "What is the name of the street where Prof. Maj's university is located?" },
-  ]);
+  const relevantTranscriptions = transcriptions.filter((_, index) => isRelevantDecisions[index]);
+  console.log(relevantTranscriptions);
+
+  const answerQuestionResponse = await openAiSkill.completionFull(
+    [
+      {
+        role: 'system',
+        content: answerQuestionPrompt(relevantTranscriptions.map((t) => `- ${t}`).join('\n')),
+      },
+      // { role: 'user', content: 'Jak się nazywa uniwersytet na którym pracował Andrzej Maj?' },
+      { role: 'user', content: 'Przy jakiej ulicy znajduje się uniwersytet na którym pracował Andrzej Maj?' },
+    ],
+    'gpt-4o',
+  );
   console.log(answerQuestionResponse.choices[0].message.content);
 };
 
