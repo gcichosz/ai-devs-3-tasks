4a1d042c6a13f3847ba164dfb543080d72966ba3
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-12 18:55:26 +0100

    feat: analyse map fragments using Open AI Vision

--- src/map/index.ts
+++ src/map/index.ts
@@ -1,6 +1,6 @@
 import { ImageManipulationSkill } from '../skills/image-manipulation/image-manipulation-skill';
+import { OpenAISkill } from '../skills/open-ai/open-ai-skill';
 
-// TODO: Use OpenAI skill to analyze map fragments
 // TODO: Setup LangChain to manage prompts
 // TODO: Use LLM to analyze map fragments and determine the most likely city based on the analysis results
 // TODO: Identify any outlier results that might be from a different city
@@ -9,9 +9,33 @@ import { ImageManipulationSkill } from '../skills/image-manipulation/image-manip
 
 async function main() {
   const imageManipulationSkill = new ImageManipulationSkill();
+  const openAiSkill = new OpenAISkill(process.env.OPENAI_API_KEY);
 
   const base64MapImage = await imageManipulationSkill.prepareImage('./src/map/maps/mapa-1.png');
-  console.log(base64MapImage);
+  // console.log(base64MapImage);
+
+  const recognizeResponse = await openAiSkill.completionFull(
+    [
+      {
+        role: 'system',
+        content: 'You are a map recognition expert. Identify the name of the streets you see on the map.',
+      },
+      {
+        role: 'user',
+        content: [
+          {
+            type: 'image_url',
+            image_url: {
+              url: `data:image/png;base64,${base64MapImage}`,
+              detail: 'high',
+            },
+          },
+        ],
+      },
+    ],
+    'gpt-4o',
+  );
+  console.log(recognizeResponse.choices[0].message.content);
 }
 
 main();
