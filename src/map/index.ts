510725639540c061ddde275b294cd82ed66d2202
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-12 18:19:08 +0100

    chore: add map task solution plan

---
+++ src/map/index.ts
@@ -0,0 +1,12 @@
+// TODO: Prepare four images of map fragments from the provided folder or physical documents
+// TODO: Create Vision skill
+// TODO: Use vision skill to analyze map fragments
+// TODO: Setup LangChain to manage prompts
+// TODO: Use LLM to analyze map fragments and determine the most likely city based on the analysis results
+// TODO: Identify any outlier results that might be from a different city
+// TODO: Determine the final city name based on the analysis and outlier detection
+// TODO: Submit the identified city name as the flag to the headquarters
+
+async function main() {}
+
+main();
