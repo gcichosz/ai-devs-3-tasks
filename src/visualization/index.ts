6cc83966d26d2d67e967eb118f5c8c56060471af
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-13 19:11:00 +0100

    feat: fetch testimony

--- src/visualization/index.ts
+++ src/visualization/index.ts
@@ -1,8 +1,20 @@
-// TODO: Fetch testimony
 // TODO: Add image generation skill
 // TODO: Generate image based on testimony
 // TODO: Report the image
 
-const main = async () => {};
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
+const fetchTestimony = async () => {
+  const sendRequestSkill = new SendRequestSkill();
+  const testimony = await sendRequestSkill.getRequest(
+    `https://centrala.ag3nts.org/data/${process.env.AI_DEVS_API_KEY}/robotid.txt`,
+  );
+  return testimony;
+};
+
+const main = async () => {
+  const testimony = await fetchTestimony();
+  console.log(testimony);
+};
 
 main();
