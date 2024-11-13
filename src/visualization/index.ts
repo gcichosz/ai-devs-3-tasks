d889f8a1724facb66251086ed88e05da8cb68e2e
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-13 19:22:29 +0100

    feat: report robot visualization

--- src/visualization/index.ts
+++ src/visualization/index.ts
@@ -1,10 +1,7 @@
-// TODO: Report the image
-
 import { ImageGenerationSkill } from '../skills/image-generation/image-generation-skill';
 import { SendRequestSkill } from '../skills/send-request/send-request-skill';
 
-const fetchTestimony = async () => {
-  const sendRequestSkill = new SendRequestSkill();
+const fetchTestimony = async (sendRequestSkill: SendRequestSkill) => {
   const testimony = await sendRequestSkill.getRequest(
     `https://centrala.ag3nts.org/data/${process.env.AI_DEVS_API_KEY}/robotid.txt`,
   );
@@ -18,11 +15,20 @@ const generateImage = async (testimony: string) => {
 };
 
 const main = async () => {
-  const testimony = await fetchTestimony();
+  const sendRequestSkill = new SendRequestSkill();
+
+  const testimony = await fetchTestimony(sendRequestSkill);
   console.log(testimony);
 
   const imageUrl = await generateImage(testimony.description as string);
   console.log(imageUrl);
+
+  const reportResponse = await sendRequestSkill.postRequest('https://centrala.ag3nts.org/report', {
+    task: 'robotid',
+    apikey: process.env.AI_DEVS_API_KEY,
+    answer: imageUrl,
+  });
+  console.log(reportResponse);
 };
 
 main();
