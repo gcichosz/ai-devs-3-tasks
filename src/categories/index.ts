6b5491955129c0c17a79b8d98701ceaebd97c627
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-14 07:50:00 +0100

    chore: add categories task solution plan

---
+++ src/categories/index.ts
@@ -0,0 +1,9 @@
+// TODO: Create function for reading file contents (excluding facts folder)
+// TODO: Create a function to convert each file type to text
+// TODO: Create function for text analysis and content classification (people/machines)
+// TODO: Implement function for alphabetical sorting of filenames
+// TODO: Create function for generating report in JSON format
+
+const main = async () => {};
+
+main();
