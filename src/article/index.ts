a5bf3358c6c7949905e1eaf8210c1c5be8037eaf
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-15 07:40:16 +0100

    chore: add article task solution plan

---
+++ src/article/index.ts
@@ -0,0 +1,12 @@
+// TODO: Index text from the article
+// TODO: Index images from the article
+// TODO: Add context to the images
+// TODO: Index audio from the article
+// TODO: Add context to the audio
+// TODO: Get article questions
+// TODO: Answer article questions using RAG prompt
+// TODO: Report answers
+
+const main = async () => {};
+
+main();
