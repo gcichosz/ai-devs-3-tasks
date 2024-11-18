bebd531b39e6173e1a8736205e001bd4f4c99802
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-18 18:16:09 +0100

    chore: add metadata task partial solution plan

---
+++ src/metadata/index.ts
@@ -0,0 +1,9 @@
+// TODO: Transcribe files
+// TODO: Convert transcriptions to files with metadata
+// TODO: Filter facts
+// TODO: Convert facts to files with metadata
+// TODO: ...
+
+async function main() {}
+
+main();
