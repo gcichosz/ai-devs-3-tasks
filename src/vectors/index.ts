10309ad7131c417273d1e1c426921bde22a199df
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-19 18:59:37 +0100

    chore: add vectors task solution plan

---
+++ src/vectors/index.ts
@@ -0,0 +1,10 @@
+// TODO: Set up a vector database (Qdrant)
+// TODO: Create embeddings for each report using OpenAI embeddings model (metadata: date, keywords?)
+// TODO: Create embedding for the search query: "W raporcie, z którego dnia znajduje się wzmianka o kradzieży prototypu broni?"
+// TODO: Search vector database with the query embedding (limit: 1)
+// TODO: Format the date as YYYY-MM-DD
+// TODO: Send the formatted date to centrala.ag3nts.org/report (task: "wektory", answer: formatted date)
+
+const main = async () => {};
+
+main();
