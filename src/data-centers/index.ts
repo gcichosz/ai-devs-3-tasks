4a6f597ff7efd75be5ea62c03fd8187184bdd4c2
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-20 19:01:57 +0100

    feat: use LLM to dynamically generate SQL query

--- src/data-centers/index.ts
+++ src/data-centers/index.ts
@@ -1,7 +1,9 @@
+import { OpenAISkill } from '../skills/open-ai/open-ai-skill';
 import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+import { LangfuseService } from '../utils/langfuse/langfuse-service';
 
-// TODO: Prepare SQL query to extract required data using an LLM
-// TODO: Send response to headquarters
+// TODO: Send query to database
+// TODO: Send report to headquarters
 
 const fetchTableStructure = async (tableName: string) => {
   const sendRequestSkill = new SendRequestSkill();
@@ -11,7 +13,31 @@ const fetchTableStructure = async (tableName: string) => {
     query: `SHOW CREATE TABLE ${tableName}`,
   });
 
-  return fetchTableStructureResponse;
+  return (fetchTableStructureResponse.reply as Array<Record<string, string>>)[0];
+};
+
+const prepareQuery = async (tables: string[]) => {
+  console.log(tables);
+  const langfuseService = new LangfuseService(
+    process.env.LANGFUSE_PUBLIC_KEY ?? '',
+    process.env.LANGFUSE_SECRET_KEY ?? '',
+  );
+  const openAiSkill = new OpenAISkill(process.env.OPENAI_API_KEY ?? '');
+
+  const sqlExpertPrompt = await langfuseService.getPrompt('data-centers-sql-expert');
+  const [sqlExpertSystemMessage] = sqlExpertPrompt.compile({ tables: tables.join('\n\n') });
+  const response = await openAiSkill.completionFull(
+    [
+      sqlExpertSystemMessage as never,
+      {
+        role: 'user',
+        content: 'które aktywne datacenter (DC_ID) są zarządzane przez pracowników, którzy są na urlopie (is_active=0)',
+      },
+    ],
+    'gpt-4o',
+  );
+
+  return response.choices[0].message.content ?? '';
 };
 
 const main = async () => {
@@ -20,6 +46,9 @@ const main = async () => {
 
   const dataCentersTable = await fetchTableStructure('datacenters');
   console.log(dataCentersTable);
+
+  const query = await prepareQuery([usersTable['Create Table'], dataCentersTable['Create Table']] as string[]);
+  console.log(query);
 };
 
 main();
