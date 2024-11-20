938aeeab2588aa97c0815893f414c7fbfca56f49
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-20 18:15:44 +0100

    feat: fetch users and datacenter tables structure

--- src/data-centers/index.ts
+++ src/data-centers/index.ts
@@ -1,7 +1,25 @@
-// TODO: Fetch required table structures (users, datacenters)
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
 // TODO: Prepare SQL query to extract required data using an LLM
 // TODO: Send response to headquarters
 
-const main = async () => {};
+const fetchTableStructure = async (tableName: string) => {
+  const sendRequestSkill = new SendRequestSkill();
+  const fetchTableStructureResponse = await sendRequestSkill.postRequest('https://centrala.ag3nts.org/apidb', {
+    task: 'database',
+    apikey: process.env.AI_DEVS_API_KEY,
+    query: `SHOW CREATE TABLE ${tableName}`,
+  });
+
+  return fetchTableStructureResponse;
+};
+
+const main = async () => {
+  const usersTable = await fetchTableStructure('users');
+  console.log(usersTable);
+
+  const dataCentersTable = await fetchTableStructure('datacenters');
+  console.log(dataCentersTable);
+};
 
 main();
