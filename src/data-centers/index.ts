c25d8b140fb1e3336055ce759cd6a7b0ac3afd10
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-20 18:09:10 +0100

    chore: add data centers task solution plan

---
+++ src/data-centers/index.ts
@@ -0,0 +1,7 @@
+// TODO: Fetch required table structures (users, datacenters)
+// TODO: Prepare SQL query to extract required data using an LLM
+// TODO: Send response to headquarters
+
+const main = async () => {};
+
+main();
