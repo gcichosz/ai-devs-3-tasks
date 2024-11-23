437b276b1f6223b957f9315ea6df15162540d445
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-23 18:34:35 +0100

    chore: add connections task solution plan

---
+++ src/connections/index.ts
@@ -0,0 +1,9 @@
+// TODO: Fetch and cache Users and Connections
+// TODO: Add Graph service
+// TODO: Build a graph of users and connections
+// TODO: Get the shortest path between two users (Rafał and Barbara)
+// TODO: Report result
+
+const main = async () => {};
+
+main();
