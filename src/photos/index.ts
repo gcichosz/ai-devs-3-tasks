425fcfce9501ef40402a8a3574f7db7f6027ba8e
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-25 18:43:59 +0100

    chore: add photos task solution plan

---
+++ src/photos/index.ts
@@ -0,0 +1,10 @@
+// TODO: Start a conversation to get the photos
+// TODO: Handle different ways of getting photos
+// TODO: Figure out what to do with each photo
+// TODO: Figure out which pictures show Barbara
+// TODO: Get Barbara's photo description
+// TODO: Report the result
+
+const main = async () => {};
+
+main();
