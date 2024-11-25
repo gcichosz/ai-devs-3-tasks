5d956ef0eb224feb5a6fe0ec14bc5dd4a424eabd
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-25 18:50:57 +0100

    fest: fetch get photos instructions

--- src/photos/index.ts
+++ src/photos/index.ts
@@ -1,10 +1,26 @@
-// TODO: Start a conversation to get the photos
 // TODO: Handle different ways of getting photos
 // TODO: Figure out what to do with each photo
 // TODO: Figure out which pictures show Barbara
 // TODO: Get Barbara's photo description
 // TODO: Report the result
 
-const main = async () => {};
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
+const startConversation = async (sendRequestSkill: SendRequestSkill) => {
+  const startResponse = await sendRequestSkill.postRequest('https://centrala.ag3nts.org/report', {
+    task: 'photos',
+    apikey: process.env.AI_DEVS_API_KEY,
+    answer: 'START',
+  });
+  console.log(startResponse);
+  return startResponse.message;
+};
+
+const main = async () => {
+  const sendRequestSkill = new SendRequestSkill();
+
+  const getPhotosInstructions = await startConversation(sendRequestSkill);
+  console.log(getPhotosInstructions);
+};
 
 main();
