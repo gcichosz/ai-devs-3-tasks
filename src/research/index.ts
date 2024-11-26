5da013eb671b526cc19cb39b8724a73353f30e6a
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-26 19:37:56 +0100

    feat: convert incorrect data to fine-tuning data

--- src/research/index.ts
+++ src/research/index.ts
@@ -1,10 +1,54 @@
-// TODO: Convert incorrect data to fine-tuning JSONL
+import { promises as fs } from 'fs';
+
 // TODO: Convert correct data to fine-tuning JSONL
 // TODO: Combine fine-tuning data
 // TODO: Fine-tune model
 // TODO: Verify data with a fine-tuned model
 // TODO: Report results
 
-const main = async () => {};
+interface ChatMessages {
+  messages: {
+    role: string;
+    content: string;
+  }[];
+}
+
+const readFile = async (filename: string) => {
+  const fileData = await fs.readFile('./src/research/lab_data/' + filename, 'utf-8');
+  return fileData.split('\n').filter((line) => line.length > 0);
+};
+
+const translateToFineTuningMessages = (data: string[], label: string) => {
+  return data.map((line) => ({
+    messages: [
+      {
+        role: 'system',
+        content: 'Verify data',
+      },
+      {
+        role: 'user',
+        content: line,
+      },
+      {
+        role: 'assistant',
+        content: label,
+      },
+    ],
+  }));
+};
+
+const saveMessages = async (messages: ChatMessages[], label: string) => {
+  await fs.writeFile(`./src/research/${label}.jsonl`, messages.map((msg) => JSON.stringify(msg)).join('\n'));
+};
+
+const main = async () => {
+  const incorrectData = await readFile('incorrect.txt');
+  console.log(JSON.stringify(incorrectData, null, 2));
+
+  const incorrectMessages = translateToFineTuningMessages(incorrectData, 'incorrect');
+  console.log(JSON.stringify(incorrectMessages, null, 2));
+
+  await saveMessages(incorrectMessages, 'incorrect');
+};
 
 main();
