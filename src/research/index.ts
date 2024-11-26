1111a71b5b237461f35142f58afbf98f257df86f
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-26 19:24:07 +0100

    chore: add research task solution plan

---
+++ src/research/index.ts
@@ -0,0 +1,10 @@
+// TODO: Convert incorrect data to fine-tuning JSONL
+// TODO: Convert correct data to fine-tuning JSONL
+// TODO: Combine fine-tuning data
+// TODO: Fine-tune model
+// TODO: Verify data with a fine-tuned model
+// TODO: Report results
+
+const main = async () => {};
+
+main();
