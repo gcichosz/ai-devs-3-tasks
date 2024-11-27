aea9aec786d37f91763345fec7404c8cf5717e5a
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-27 17:37:09 +0100

    chore: add softo task solution plan

---
+++ src/softo/index.ts
@@ -0,0 +1,10 @@
+// TODO: Fetch questions
+// TODO: Scrape main(?) page
+// TODO: Try answering questions based on the scraped page
+// TODO: Pick best link to follow (rank potential links)
+// TODO: Add visited links tracking
+// TODO: Save visited links summary and their links
+
+const main = async () => {};
+
+main();
