78f44188e410a07fc7427062d45cbb4c2402bc36
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-27 17:40:01 +0100

    feat: fetch softo questions

--- src/softo/index.ts
+++ src/softo/index.ts
@@ -1,10 +1,22 @@
-// TODO: Fetch questions
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
 // TODO: Scrape main(?) page
 // TODO: Try answering questions based on the scraped page
 // TODO: Pick best link to follow (rank potential links)
 // TODO: Add visited links tracking
 // TODO: Save visited links summary and their links
 
-const main = async () => {};
+const fetchQuestions = async (sendRequestSkill: SendRequestSkill) => {
+  return await sendRequestSkill.getRequest(
+    `https://centrala.ag3nts.org/data/${process.env.AI_DEVS_API_KEY}/softo.json`,
+  );
+};
+
+const main = async () => {
+  const sendRequestSkill = new SendRequestSkill();
+
+  const questions = await fetchQuestions(sendRequestSkill);
+  console.log(questions);
+};
 
 main();
