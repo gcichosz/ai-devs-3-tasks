a5588002f09020f938a9c6857dcf641bdeb785ab
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-11-30 16:24:21 +0100

    feat: add a simple Express server for webhook

--- src/webhook/index.ts
+++ src/webhook/index.ts
@@ -1,3 +1,21 @@
-// TODO: Create Express server with JSON body parser
+import bodyParser from 'body-parser';
+import express from 'express';
+
 // TODO: Read instruction and pass it to LLM to get location
 // TODO: Return LLM response to the client in description field
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  console.log(req.body);
+  const instruction = req.body.instruction;
+  console.log(instruction);
+  res.json({ description: '' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
