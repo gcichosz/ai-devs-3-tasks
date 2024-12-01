9b4042482da38d95693fc1551658cb8e91c93593
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-12-01 17:19:34 +0100

    feat: convert PDF notes to text

--- src/notes/index.ts
+++ src/notes/index.ts
@@ -1,9 +1,18 @@
-// TODO: OCR notes
+import { readPdfText } from 'pdf-text-reader';
+
 // TODO: ORC notes last page using LLM
 // TODO: Get questions
 // TODO: Answer questions using answer with context prompt (notes are the context)
 // TODO: Report results
 
-const main = async () => {};
+const readPdfNotes = async () => {
+  const pdfNotes = await readPdfText({ url: './src/notes/notatnik-rafala.pdf' });
+  return pdfNotes;
+};
+
+const main = async () => {
+  const pdfNotes = await readPdfNotes();
+  console.log(pdfNotes);
+};
 
 main();
