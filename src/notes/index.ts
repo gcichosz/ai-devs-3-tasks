01e3e64c174021290921ede28a782c1c34023c03
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-12-01 17:11:14 +0100

    chore: add notes task solution plan

---
+++ src/notes/index.ts
@@ -0,0 +1,9 @@
+// TODO: OCR notes
+// TODO: ORC notes last page using LLM
+// TODO: Get questions
+// TODO: Answer questions using answer with context prompt (notes are the context)
+// TODO: Report results
+
+const main = async () => {};
+
+main();
