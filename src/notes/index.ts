25087d86d2f23ccb86781b1833a04f97d87b1820
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-12-01 17:43:51 +0100

    feat: get notes questions

--- src/notes/index.ts
+++ src/notes/index.ts
@@ -1,7 +1,9 @@
 import { readPdfText } from 'pdf-text-reader';
 
-// TODO: ORC notes last page using LLM
-// TODO: Get questions
+import { ImageManipulationSkill } from '../skills/image-manipulation/image-manipulation-skill';
+import { OpenAISkill } from '../skills/open-ai/open-ai-skill';
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+
 // TODO: Answer questions using answer with context prompt (notes are the context)
 // TODO: Report results
 
@@ -10,9 +12,28 @@ const readPdfNotes = async () => {
   return pdfNotes;
 };
 
+// TODO: ORC notes last page
+const readPngText = async (imageManipulationService: ImageManipulationSkill, openAiService: OpenAISkill) => {
+  const image = await imageManipulationService.prepareImageFromPath('./src/notes/ostatnia-strona.png');
+  const text = await openAiService.vision({ role: 'system', content: 'Extract text from image' }, [image], 'gpt-4o');
+  return text;
+};
+
 const main = async () => {
+  const imageManipulationService = new ImageManipulationSkill();
+  const openAiService = new OpenAISkill(process.env.OPENAI_API_KEY);
+  const sendRequestSkill = new SendRequestSkill();
+
   const pdfNotes = await readPdfNotes();
   console.log(pdfNotes);
+
+  // const pngNotes = await readPngText(imageManipulationService, openAiService);
+  // console.log(pngNotes);
+
+  const questions = await sendRequestSkill.getRequest(
+    `https://centrala.ag3nts.org/data/${process.env.AI_DEVS_API_KEY}/notes.json`,
+  );
+  console.log(questions);
 };
 
 main();
