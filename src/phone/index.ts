80d2b76a3326110904b42c70245313803f6c9ac4
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-12-15 14:26:00 +0100

    feat: identify speakers in phone task

--- src/phone/index.ts
+++ src/phone/index.ts
@@ -1,13 +1,13 @@
 import { promises as fs } from 'fs';
 import { v4 as uuid } from 'uuid';
 
+import { OpenAISkill } from '../skills/open-ai/open-ai-skill';
 import { SendRequestSkill } from '../skills/send-request/send-request-skill';
+import { LangfuseService } from '../utils/langfuse/langfuse-service';
 import { loadFromCache } from './cache-service';
 import { Document } from './types';
 
 // Min
-// TODO: Identify speakers in each conversation
-
 // TODO: Create function to extract facts stated by each speaker
 // TODO: Cross-reference facts with known facts from previous tasks
 // TODO: Identify the liar by finding contradictions
@@ -45,7 +45,8 @@ const getConversations = async (sendRequestSkill: SendRequestSkill): Promise<Doc
       },
     })) as Document[];
   };
-  return loadFromCache('./src/phone/conversations', getOriginalConversations);
+  const conversations = await loadFromCache('./src/phone/conversations', getOriginalConversations);
+  return conversations.sort((a, b) => (a.metadata.name as string).localeCompare(b.metadata.name as string));
 };
 
 const loadFacts = async (): Promise<Document[]> => {
@@ -73,8 +74,58 @@ const getQuestions = async (sendRequestSkill: SendRequestSkill) => {
   );
 };
 
+const identifySpeakers = async (
+  conversations: Document[],
+  facts: Document[],
+  langfuseService: LangfuseService,
+  openAIService: OpenAISkill,
+) => {
+  const getSpeakers = async () => {
+    const identifySpeakersPrompt = await langfuseService.getPrompt('identify-speakers');
+    const [identifySpeakersPromptMessage] = identifySpeakersPrompt.compile({
+      context: facts.map((f) => `<fact name="${f.metadata.name}">${f.text}</fact>`).join('\n'),
+    });
+
+    const conversationsContent = conversations
+      .map((c) => `<${c.metadata.name}>${c.text}\n</${c.metadata.name}>`)
+      .join('\n');
+    const speakersResponse = await openAIService.completionFull(
+      [
+        identifySpeakersPromptMessage as never,
+        {
+          role: 'user',
+          content: conversationsContent,
+        },
+      ],
+      'gpt-4o',
+    );
+
+    const speakersAnswer = speakersResponse.choices[0].message.content;
+    const finalAnswer = speakersAnswer?.match(/<final_answer>(.*?)<\/final_answer>/s)?.[1].trim();
+    const identifiedConversations = [];
+    for (let i = 0; i < conversations.length; i++) {
+      const identifiedConversation = finalAnswer
+        ?.match(new RegExp(`<rozmowa${i + 1}>(.*?)</rozmowa${i + 1}>`, 's'))?.[1]
+        .trim();
+      identifiedConversations.push({
+        uuid: uuid(),
+        text: identifiedConversation!,
+        metadata: {
+          name: `rozmowa${i + 1}`,
+        },
+      });
+    }
+
+    return identifiedConversations;
+  };
+
+  return await loadFromCache('./src/phone/speakers', getSpeakers);
+};
+
 const main = async () => {
   const sendRequestSkill = new SendRequestSkill();
+  const langfuseService = new LangfuseService(process.env.LANGFUSE_PUBLIC_KEY, process.env.LANGFUSE_SECRET_KEY);
+  const openAIService = new OpenAISkill(process.env.OPENAI_API_KEY);
 
   const anonymousConversations = await getConversations(sendRequestSkill);
   console.log('Anonymous conversations:', anonymousConversations);
@@ -84,6 +135,9 @@ const main = async () => {
 
   const questions = await getQuestions(sendRequestSkill);
   console.log('Questions:', questions);
+
+  const conversations = await identifySpeakers(anonymousConversations, facts, langfuseService, openAIService);
+  console.log('Conversations:', conversations);
 };
 
 main();
