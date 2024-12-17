6c3b28ab57e4b434ef553157bae79e0458288239
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2024-12-17 19:30:00 +0100

    chore:(pgs) add gps task solution plan

---
+++ src/gps/index.ts
@@ -0,0 +1,14 @@
+// TODO: Fetch the input data
+// TODO: Create main agent loop skeleton
+// TODO: Add initial plan 'tool'
+// TODO: Add select next step 'tool'
+// TODO: Add final answer tool
+// TODO: Add query DB tool (get user id)
+// TODO: Add call API tool (get location users)
+// TODO: Add check user coordinates tool
+// TODO: Remove information about Barbara
+// TODO: Report result
+
+const main = async () => {};
+
+main();
