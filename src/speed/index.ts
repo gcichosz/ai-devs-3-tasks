c7d1179b7814618c61ef7651c4cf8e21b80a93dd
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2025-01-13 07:17:20 +0100

    feat(speed): get hash

--- src/speed/index.ts
+++ src/speed/index.ts
@@ -1,7 +1,4 @@
-// TODO: Define constants for API endpoints (rafal.ag3nts.org)
-// TODO: Define interface types for API responses
-
-// TODO: Step 1 - Send initial request with password "NONOMNISMORIAR" to get hash
+import { SendRequestSkill } from '../skills/send-request/send-request-skill';
 
 // TODO: Step 2 - Send hash in "sign" field to get:
 //       - timestamp
@@ -30,6 +27,21 @@
 
 // TODO: Add error handling and timeout management (6 second limit)
 
-const main = async () => {};
+const mainEndpoint = 'https://rafal.ag3nts.org/b46c3';
+
+const getHash = async (sendRequestSkill: SendRequestSkill) => {
+  const response = await sendRequestSkill.postRequest(mainEndpoint, {
+    password: 'NONOMNISMORIAR',
+  });
+  console.log(response);
+  return response.message;
+};
+
+const main = async () => {
+  const sendRequestSkill = new SendRequestSkill();
+
+  const hash = await getHash(sendRequestSkill);
+  console.log('#️⃣ Hash:', hash);
+};
 
 main();
