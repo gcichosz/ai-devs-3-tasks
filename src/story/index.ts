75c4ff2119e8baf935c32140bdcc86f6572da975
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2025-01-17 08:04:10 +0100

    chore(story): add story rough solution plan

---
+++ src/story/index.ts
@@ -0,0 +1,10 @@
+// TODO: Setup vector database
+// TODO: Create a function to chunk content
+// TODO: Ingest content into vector database (create embeddings)
+// TODO: Create a function to query the vector database (with re-ranking?)
+// TODO: Create a function to answer the question
+// TODO: Create a function to submit the answer
+
+const main = async () => {};
+
+main();
