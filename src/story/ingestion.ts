5612d46e5004d05060f4dbbe6f506bd2e87d608c
Grzegorz Cichosz <grzegorzcichosz2@gmail.com>
2025-01-19 15:06:41 +0100

    feat(story): ingest Softo website

--- src/story/ingestion.ts
+++ src/story/ingestion.ts
@@ -40,9 +40,11 @@ const ingestBlog = async (
   console.log('🪓 Splitting blog');
   const docs = await textSplitter.split(markdown, 1000, 'blog_rafala');
   console.log('🪓 Blog splitted');
-  for (const doc of docs) {
-    await saveDocument(doc, openAiSkill, qdrantService);
-  }
+  await Promise.all(
+    docs.map(async (doc) => {
+      await saveDocument(doc, openAiSkill, qdrantService);
+    }),
+  );
 };
 
 const ingestNotes = async (
@@ -76,6 +78,42 @@ const ingestNotes = async (
   );
 };
 
+const ingestWebsite = async (
+  scrapeWebSkill: ScrapeWebSkill,
+  openAiSkill: OpenAISkill,
+  qdrantService: QdrantService,
+) => {
+  const pages = [
+    'https://softo.ag3nts.org/',
+    'https://softo.ag3nts.org/uslugi',
+    'https://softo.ag3nts.org/portfolio',
+    'https://softo.ag3nts.org/portfolio_1_c4ca4238a0b923820dcc509a6f75849b',
+    'https://softo.ag3nts.org/portfolio_2_c81e728d9d4c2f636f067f89cc14862c',
+    'https://softo.ag3nts.org/portfolio_3_eccbc87e4b5ce2fe28308fd9f2a7baf3',
+    'https://softo.ag3nts.org/portfolio_4_a87ff679a2f3e71d9181a67b7542122c',
+    'https://softo.ag3nts.org/portfolio_6_1679091c5a880faf6fb5e6087eb1b2dc',
+    'https://softo.ag3nts.org/aktualnosci',
+    'https://softo.ag3nts.org/blog/automatyzacja-procesow',
+    'https://softo.ag3nts.org/blog/sukcesy-softoai',
+    'https://softo.ag3nts.org/blog/nowe-modele-llm',
+    'https://softo.ag3nts.org/kontakt',
+  ];
+
+  for (const page of pages) {
+    console.log(`🔍 Scraping page: ${page}`);
+    const { markdown } = await scrapeWebSkill.scrapeUrl(page);
+    console.log(`🔍 Page scraped: ${page}`, markdown);
+    const document = {
+      uuid: uuidv4(),
+      text: markdown,
+      metadata: {
+        type: 'strona_softo',
+      },
+    };
+    await saveDocument(document, openAiSkill, qdrantService);
+  }
+};
+
 const ingest = async () => {
   const qdrantService = new QdrantService(process.env.QDRANT_URL, process.env.QDRANT_API_KEY);
   const scrapeWebSkill = new ScrapeWebSkill(process.env.FIRECRAWL_API_KEY!, [
@@ -83,16 +121,21 @@ const ingest = async () => {
       name: 'blog',
       url: 'https://rafal.ag3nts.org/',
     },
+    {
+      name: 'softo',
+      url: 'https://softo.ag3nts.org/',
+    },
   ]);
   const textSplitter = new TextSplitter();
   await textSplitter.initializeTokenizer();
   const openAiSkill = new OpenAISkill(process.env.OPENAI_API_KEY!);
   const imageManipulationSkill = new ImageManipulationSkill();
 
-  await qdrantService.createCollection(QDRANT_COLLECTION, 3072, true);
+  await qdrantService.createCollection(QDRANT_COLLECTION, 3072);
 
   await ingestBlog(scrapeWebSkill, textSplitter, openAiSkill, qdrantService);
   await ingestNotes(imageManipulationSkill, openAiSkill, qdrantService);
+  await ingestWebsite(scrapeWebSkill, openAiSkill, qdrantService);
 };
 
 ingest();
